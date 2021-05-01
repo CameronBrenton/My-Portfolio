1f51f346a051d58bff0dfd954568c4c04ab8603f
CameronBrenton <Cameron.Brenton@keyin.com>
2021-05-01 16:05:31 -0230

    4:06 pm

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function main() {
   
   window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * 0.1 + "px";
+    //parallax.style.backgroundPositionY = offset * 0.1 + "px";
   });
 }
 window.addEventListener("load", main);
