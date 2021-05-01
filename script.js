937e2532ba2c23a773f0441bca3773269c09f5ea
CameronBrenton <Cameron.Brenton@keyin.com>
2021-05-01 19:12:07 -0230

    More progress with the portfolio in general and the style is almost done, now time to focus on displaying example work.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function main() {
-  let parallax = document.querySelector("#parallax-2");
+  let parallax = document.querySelector("#parallax-8");
   
   window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
-    //parallax.style.backgroundPositionY = offset * 0.1 + "px";
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
   });
 }
 window.addEventListener("load", main);
