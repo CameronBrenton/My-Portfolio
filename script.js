b478df72fa52b207fd16ae580435b69dfb9ba912
CameronBrenton <Cameron.Brenton@keyin.com>
2021-05-01 12:49:34 -0230

    changed parallax methods

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
 function main() {
-  let backgroundImage = document.querySelector("#backgroundImage");
-  let backgroundImage2 = document.querySelector('#backgroundImage2');
-
-  function setTranslate(xPos, yPos, el) {
-    el.style.transform = "translate3d(" + xPos + "px, " 
-                          + yPos + "px, 0)";
-  }
- 
-  let xScrollPosition;
-  let yScrollPosition;
- 
-  function scrollLoop() {
-    console.log("test3")
-    xScrollPosition = window.scrollX;
-    yScrollPosition = window.scrollY;
-
-    setTranslate(0, yScrollPosition * -0.2, backgroundImage);
-    setTranslate(0, yScrollPosition * -0.2, backgroundImage2);
+  let parallax = document.querySelector("#parallax-2");
   
-    requestAnimationFrame(scrollLoop);
-  }
-  
-  scrollLoop();
+  window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+  });
 }
 window.addEventListener("load", main);
